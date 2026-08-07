fbe10b86029967d1f7b24803970c8f2c9a1a53e8
pvlist <patrickvandervlist@gmail.com>
2026-08-07 22:16:17 +0200

    Created javascript file to edit the events seperatly

---
+++ events.js
@@ -0,0 +1,44 @@
+const events = [
+  {
+    title: "Family Day",
+    date: "2026-08-15T10:00:00",
+    location: "Zilvermeerlaan 2, 2400 Mol, België",
+    description: "Familiedag is dè jaarlijkse traditie waar we genieten van heerlijk eten, zwemmen en leuke momenten",
+    image: "https://www.zilvermeer.be/sites/zilvermeer/files/styles/drimage_improved_focal_2800_933/public/2025-07/tk--camping--zilvermeer-243-web.jpg.webp"
+  },
+  {
+    title: "Open meeting",
+    date: "2026-08-27T19:30:00",
+    location: "Oude Lieshoutseweg 7, 5492 HT Sint-Oedenrode",
+    description: "De maandelijkse open meeting",
+    image: "https://wwsg.com/wp-content/uploads/2023/05/Speaker-on-small-stage-1024x576.jpg"
+  },
+  {
+    title: "Open meeting",
+    date: "2026-09-24T19:30:00",
+    location: "TBD",
+    description: "De maandelijkse open meeting",
+    image: "https://wwsg.com/wp-content/uploads/2023/05/Speaker-on-small-stage-1024x576.jpg"
+  },
+  {
+    title: "Open meeting",
+    date: "2026-10-29T19:30:00",
+    location: "Oude Lieshoutseweg 7, 5492 HT Sint-Oedenrode",
+    description: "De maandelijkse open meeting",
+    image: "https://wwsg.com/wp-content/uploads/2023/05/Speaker-on-small-stage-1024x576.jpg"
+  },
+  {
+    title: "Open meeting",
+    date: "2026-11-26T19:30:00",
+    location: "Oude Lieshoutseweg 7, 5492 HT Sint-Oedenrode",
+    description: "De maandelijkse open meeting",
+    image: "https://wwsg.com/wp-content/uploads/2023/05/Speaker-on-small-stage-1024x576.jpg"
+  },
+  {
+    title: "Powerboost",
+    date: "2026-08-29T11:00:00",
+    location: "Zoom",
+    description: "Een training waar tips en tricks jouw letterlijk een boost geven",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT_fNrFOLqjeU4CY4GxS4SXrJHkcjzZmFzLNoxy8divQ&s=10"
+  }
+];
